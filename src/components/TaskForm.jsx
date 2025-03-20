3e66514b1d85fff41715dc9db4768b8891a018cd
Noorullah Noori <164587782+noorinoorullah130@users.noreply.github.com>
2025-03-20 13:28:08 +0430

    useState are added and some more features

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TaskForm.css";
 import Tag from "./Tag";
 
 const TaskForm = () => {
+    const [taskData, setTaskData] = useState({
+        task: "",
+        status: "todo",
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setTaskData((prev) => {
+            return { ...prev, [name]: value };
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(taskData);
+    };
+    
+    console.log(taskData);
+    
     return (
         <header className="app_header">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input
                     type="text"
+                    name="task"
+                    onChange={handleChange}
                     className="task_input"
                     placeholder="Enter your task"
                 />
@@ -20,7 +41,11 @@ const TaskForm = () => {
                     </div>
 
                     <div>
-                        <select className="task_status">
+                        <select
+                            className="task_status"
+                            onChange={handleChange}
+                            name="status"
+                        >
                             <option value="todo">To Do</option>
                             <option value="doing">Doing</option>
                             <option value="done">Done</option>
